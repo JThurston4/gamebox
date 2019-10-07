7b5c02d224a7ff7e6e59828e15b28f7c3d381734
Joshua Thurston <joshthurston92@gmail.com>
2019-10-06 21:30:41 -0600

    playing around with different things

--- src/views/Connect4.vue
+++ src/views/Connect4.vue
@@ -14,14 +14,44 @@
         <button v-on:click="newGame()">New Game</button>
       </div>
     </div>
-    <div class="container table">
-      <div v-for="(row, i) in getGame.board">
-        <div v-for="(element, j) in row" v-on:click="updateBoard(j)">
-          <div class="square">
-            <div :class="[getGame.board[i][j] == ' ' ? 'circle' : 'circle-' + getGame.board[i][j]]"></div>
-          </div>
-          <!-- <span v-if="getGame.board[i][j] == ' ' && getGame.active"
+    <div class="container table2">
+      <!-- <div v-for="(ele, k) in getGame.board[0]" v-if="getGame.board">
+        <div :class="['square2']">
+          <div :class="[playerOneTurn ? 'circle-1-' + k : 'circle-2-' + k]"></div>
+        </div>
+      </div> -->
+      <div class="yourBoat">
+        <div :class="['square2']">
+          <div :class="[getGame.playerOneTurn ? 'zero-1' : 'zero-2']"></div>
+        </div>
+        <div class="square2 one">
+          <div :class="[getGame.playerOneTurn ? 'one-1' : 'one-2']"></div>
+        </div>
+        <div class="square2 two">
+          <div :class="[getGame.playerOneTurn ? 'two-1' : 'two-2']"></div>
+        </div>
+        <div class="square2 three">
+          <div :class="[getGame.playerOneTurn ? 'three-1' : 'three-2']"></div>
+        </div>
+        <div class="square2 four">
+          <div :class="[getGame.playerOneTurn ? 'four-1' : 'four-2']"></div>
+        </div>
+        <div class="square2 five">
+          <div :class="[getGame.playerOneTurn ? 'five-1' : 'five-2']"></div>
+        </div>
+        <div class="square2 six">
+          <div :class="[getGame.playerOneTurn ? 'six-1' : 'six-2']"></div>
+        </div>
+      </div>
+      <div class="container table">
+        <div v-for="(row, i) in getGame.board">
+          <div v-for="(element, j) in row" v-on:click="updateBoard(j)">
+            <div class="square">
+              <div :class="[getGame.board[i][j] == ' ' ? 'circle' : 'circle-' + getGame.board[i][j]]"></div>
+            </div>
+            <!-- <span v-if="getGame.board[i][j] == ' ' && getGame.active"
             :class="['hov']">{{getGame.playerOneTurn === true ? 'X' : 'O'}}</span> -->
+          </div>
         </div>
       </div>
     </div>
@@ -75,6 +105,12 @@
     padding: 0px;
   }
 
+  .table2 {
+    height: 50px;
+    width: 350px;
+    padding: 0px;
+  }
+
   .circle {
     height: 35px;
     width: 35px;
@@ -110,10 +146,26 @@
     outline: 1px, solid, black;
   }
 
+  .square2 {
+    height: 50px;
+    width: 50px;
+    background: rgb(121, 121, 251);
+    float: left;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    outline: 1px, solid, black;
+  }
+
 
   @media (min-width: 768px) {
     .table {
       height: 480px;
+      width: 560px;
+    }
+
+    .table2 {
+      height: 80px;
       width: 560px;
     }
 
@@ -151,5 +203,16 @@
       align-items: center;
       outline: 1px, solid, black;
     }
+    .square2 {
+      height: 80px;
+      width: 80px;
+      background: rgb(121, 121, 251);
+      float: left;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      outline: 1px, solid, black;
+    }
+    
   }
 </style>
